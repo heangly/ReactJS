3da0787be35072296a103993b8d959fa256d5016
Heang Ly <heanglypa@gmail.com>
2020-06-29 09:58:30 -0400

    completed exercise

---
+++ color-boxes-exercise/README.md
@@ -0,0 +1,1 @@
+This is a simple color boxes exercise for practicing React state.
---
+++ color-boxes-exercise/src/Box.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react'
+import choice from './helper';
+import './Box.css';
+
+class Box extends Component{
+  constructor(props){
+    super(props);
+    this.state = {color: choice(this.props.colors)};
+    this.handleClick = this.handleClick.bind(this);
+    this.pickColor = this.pickColor.bind(this);
+  }
+
+  pickColor(){
+    let newColor;
+    do {
+      newColor = choice(this.props.colors);
+    } while (newColor === this.state.color)
+
+    this.setState({color: newColor});
+  }
+
+  handleClick(){
+    this.pickColor()
+  }
+
+  render(){
+    return(
+      <div 
+        className="Box" 
+        style={{backgroundColor: this.state.color}} 
+        onClick={this.handleClick}
+      >
+
+      </div>
+    )
+  }
+}
+
+export default Box
