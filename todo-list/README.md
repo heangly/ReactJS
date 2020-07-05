26551ff9c98b95f86b98f3a6d3af18881382a335
Heang Ly <heanglypa@gmail.com>
2020-07-05 12:32:07 -0400

    react-todo-list

---
+++ todo-list/README.md
@@ -0,0 +1,9 @@
+## Programmer 
+- Heang Ly
+- July, 2020
+
+## Todo List
+- This is a todo app that allows users to see, add, edit, and remove todos. 
+
+## Technologies
+- React Js
---
+++ todo-list/src/TodoList.js
@@ -0,0 +1,76 @@
+import React, {Component} from 'react';
+import Todo from './Todo';
+import TodoForm from './TodoForm';
+import './TodoList.css';
+
+class TodoList extends Component{
+  constructor(props){
+    super(props);
+    this.state = {todos: []}
+    this.create = this.create.bind(this);
+    this.remove = this.remove.bind(this);
+    this.update = this.update.bind(this);
+    this.toggleCompletion = this.toggleCompletion.bind(this);
+  };
+
+  create(newTodo){
+    this.setState({
+      todos:[...this.state.todos, newTodo]
+    });
+  }
+
+  remove(id){
+    this.setState({
+      todos: this.state.todos.filter(t => t.id !== id)
+    });
+  }
+
+  update(id, updatedTask){
+    const updatedTodos = this.state.todos.map(todo => {
+      if(todo.id === id){
+        return{...todo, task: updatedTask};
+      }
+      return todo;
+    });
+    this.setState({todos: updatedTodos});
+  } 
+
+  toggleCompletion(id){
+    const updatedTodos = this.state.todos.map(todo => {
+      if(todo.id === id){
+        return{...todo, completed: !todo.completed};
+      }
+      return todo;
+    });
+    this.setState({todos: updatedTodos});
+  }
+
+
+  render(){
+    const todos = this.state.todos.map(todo => {
+      return (
+        <Todo 
+          key={todo.id} 
+          id={todo.id} 
+          task={todo.task} 
+          completed={todo.completed}
+          removeTodo={this.remove}
+          updateTodo={this.update}
+          toggleTodo={this.toggleCompletion}
+        />
+      );
+    });
+
+    return(
+      <div className='TodoList'>
+        <h1>Todo List <span>A Simple React Todo List App</span></h1>
+        <ul>
+          {todos}
+          <TodoForm createTodo={this.create}/>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default TodoList;
