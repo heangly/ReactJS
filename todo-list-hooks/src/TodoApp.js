4efcf8b31f30974d2baad72354143a127291cd2c
Heang Ly <heanglypa@gmail.com>
2020-08-25 08:14:34 -0400

    finished

--- todo-list-hooks/src/TodoApp.js
+++ todo-list-hooks/src/TodoApp.js
@@ -9,12 +9,8 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Grid from "@material-ui/core/Grid";
 
 const TodoApp = () => {
-  const initialTodos = JSON.parse(window.localStorage.getItem('todos') || '[]')
+  const initialTodos = [{id:1, task:'learn programming', completed:false}];
   const {todos, addTodo, removeTodo, toggleTodo, editTodo} = useTodoState(initialTodos);
-
-  useEffect(() => {
-    window.localStorage.setItem('todos', JSON.stringify(todos));
-  }, [todos])
 
   return(
     <Paper
